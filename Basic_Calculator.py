e8182b6c3853e9f57c16451d85250790f745565d
Vikas <vikasgowda189@gmai.com>
2023-04-12 11:32:20 +0530

    Basic Calci

---
+++ Basic_Calculator.py
@@ -0,0 +1,15 @@
+print("-"*50)
+print("Basic Calculator")
+print("-"*50)
+num1 = int(input("Enter the first value :"))
+operator = input("Enter the operator ")
+num2 = int(input("Enter the second value :"))
+
+if operator == '+':
+    print("The addition is: ", num1 + num2)
+elif operator == '-':
+    print("The subracted value: ", num1 - num2)
+elif operator == '*':
+    print("The multiplied value: ", num1 * num2)
+elif operator == '/':
+    print("The Division value: ", num1 / num2)
